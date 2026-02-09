21022973c28defe8555b27b23c5bb57abc1f92f8
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2026-02-09 08:54:27 +0800

    Initial setup for project and app level templates.

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'recipebook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [], # TODO: Add template directory
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
