acf3714e2e02bf7a8b33aa36a398ae27ecfcd598
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2026-02-09 09:52:45 +0800

    Fixed type quanity to quantity in context.

--- ledger/views.py
+++ ledger/views.py
@@ -50,7 +50,7 @@ context = {
                     },
                     {
                         "name": "water",
-                        "quanity": "1 cup"
+                        "quantity": "1 cup"
                     },
                     {
                         "name": "salt",
