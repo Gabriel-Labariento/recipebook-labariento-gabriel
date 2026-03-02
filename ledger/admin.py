c874ad9a550aead0aacf338884e1bd83fb3f0acb
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2026-03-02 08:13:24 +0800

    Added user and profile to admin.

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-from .models import Recipe, RecipeIngredient
+from .models import Profile, Recipe, RecipeIngredient
 
 # Register your models here.
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline,]
 
 
 class RecipeIngredientInline(admin.TabularInline):
@@ -15,3 +26,5 @@ class RecipeAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Recipe, RecipeAdmin)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
